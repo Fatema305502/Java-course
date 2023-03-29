1306fda74c37235cefb30202470e892c707eb8c8
Fatema305502 <114063036+Fatema305502@users.noreply.github.com>
2023-03-29 08:52:57 +0200

    Initial Commit

--- src/HelloWorld/Hello.java
+++ src/HelloWorld/Hello.java
@@ -8,47 +8,28 @@ public class Hello
   {
     System.out.println("HelloWorld.Hello, Tim");
     boolean isAlien = false;
-    if (isAlien == false){
-      System.out.println("It is not an alien");
-      System.out.println("And i am scared of aliens");
-    }
+    System.out.println("It is not an alien");
+    System.out.println("And i am scared of aliens");
     int topScore =80;
-    if(topScore < 100){
-      System.out.println("You got the high score");
-    }
+    System.out.println("You got the high score");
     int secondTopScore =95;
-    if (topScore > secondTopScore && topScore<100){
-      System.out.println("Greater than second top score and less than 100");
-    }
 
-    if ((topScore >90)||(secondTopScore <= 90)){
-      System.out.println("Either or both of the conditions are true");
-    }
     int newValue= 50;
-    if (newValue ==50){
-      System.out.println("This is an error ");
-    }
+    System.out.println("This is an error ");
     boolean isCar =false;
-    if (isCar != false){
-      System.out.println("This is not supposed to happen ");
-    }
 
     String makeOfCar ="Volkswagen";
 
     //use ternary operator means (3 part )
     // we have 3 operands
-    boolean isDomestic = makeOfCar == "Volkswagen" ? false :true;
+    boolean isDomestic = false;
 
-    if (isDomestic){
-      System.out.println("This car is domestic to oue country ");
-    }
-
-    String s= (isDomestic) ?"This car is domestic " : "This car is not domestic";
+    String s= "This car is not domestic";
     System.out.println(s);
 
 
     int ageOfClient= 20;
-    String ageText= (ageOfClient >=18) ?"Over Eighteen " : "Still a kid ";
+    String ageText= "Over Eighteen ";
     System.out.println("Our client is " + ageText);
     /*
     //use ternary operator means (3 part )
